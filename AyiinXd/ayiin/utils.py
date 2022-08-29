ce23e3fa20ccf06e1a2b6b17bddf1e84dd265aa5
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-29 21:42:17 +0700

    Update utils.py

--- AyiinXd/ayiin/utils.py
+++ AyiinXd/ayiin/utils.py
@@ -174,7 +174,7 @@ async def autobot():
             await bot.send_message(bf, f"@{username}")
             await asyncio.sleep(1)
             await bot.send_message(
-                bf, f"✨ Owner ~ {who.first_name} ✨\n\n✨ Powered By ~ @AyiinSupport ✨"
+                bf, f"✨ Owner ~ {who.first_name} ✨\n\n✨ Powered By ~ @PalelSupportCH ✨"
             )
             await bot.send_message(
                 BOTLOG_CHATID,
